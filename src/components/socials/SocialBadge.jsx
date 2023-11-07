99746f212c4ca76e356492bb58c1e882683f44c1
John Paul N. Cervantes <johnpaulncervantes@gmail.com>
2023-11-07 21:57:56 +0800

    Added About Me page

--- src/components/socials/SocialBadge.jsx
+++ src/components/socials/SocialBadge.jsx
@@ -1,61 +1,62 @@
-import React from "react"
-import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
-import {BsArrowUpRight, BsFacebook, BsGithub} from "react-icons/bs"
-const SocialBadge = props => {
-    const {social} = props
+import React from "react";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsArrowUpRight, BsFacebook } from "react-icons/bs";
+const SocialBadge = (props) => {
+  const { social } = props;
 
-    const responsiveClasses = "xs:text-xs sm:text-xs md:text-xs lg:text-xs xl:text-sm"
+  const responsiveClasses =
+    "xs:text-xs sm:text-xs md:text-xs lg:text-xs xl:text-sm";
 
-    const handleSocialIcon = social => {
-        switch (social) {
-            case "LinkedIn":
-                return (
-                    <span className="h-[20px]">
-                        <AiFillLinkedin className="h-full" size={"20px"} />
-                    </span>
-                )
-            case "Facebook":
-                return (
-                    <span className="h-[20px]">
-                        <BsFacebook className="h-full" size={"20px"} />
-                    </span>
-                )
-            case "GitHub":
-                return (
-                    <span className="h-[25px]">
-                        <AiFillGithub size={"25px"} />
-                    </span>
-                )
-            default:
-                return <></>
-        }
+  const handleSocialIcon = (social) => {
+    switch (social) {
+      case "LinkedIn":
+        return (
+          <span className="h-[20px]">
+            <AiFillLinkedin className="h-full" size={"20px"} />
+          </span>
+        );
+      case "Facebook":
+        return (
+          <span className="h-[20px]">
+            <BsFacebook className="h-full" size={"20px"} />
+          </span>
+        );
+      case "GitHub":
+        return (
+          <span className="h-[25px]">
+            <AiFillGithub size={"25px"} />
+          </span>
+        );
+      default:
+        return <></>;
     }
+  };
 
-    const handleRedirectLink = social => {
-        switch (social) {
-            case "LinkedIn":
-                return "https://www.linkedin.com/in/johnpaulncervantes/"
-            case "Facebook":
-                return "https://www.facebook.com/cervantesjohnpauln/"
-            case "GitHub":
-                return "https://github.com/jpncervantes?tab=repositories"
-            default:
-                return <></>
-        }
+  const handleRedirectLink = (social) => {
+    switch (social) {
+      case "LinkedIn":
+        return "https://www.linkedin.com/in/johnpaulncervantes/";
+      case "Facebook":
+        return "https://www.facebook.com/cervantesjohnpauln/";
+      case "GitHub":
+        return "https://github.com/jpncervantes?tab=repositories";
+      default:
+        return <></>;
     }
-    return (
-        <a href={handleRedirectLink(social)} target="_blank">
-            <div
-                className={`bg-[#1F1F1F] rounded-full text-white flex items-center justify-center cursor-pointer relative h-[35px] gap-3 p-2 ${responsiveClasses}`}
-            >
-                {handleSocialIcon(social)}
-                <span className="hidden md:flex">{social}</span>
-                <span className="hidden lg:flex">
-                    <BsArrowUpRight className="top-2 right-4" size={15} />
-                </span>
-            </div>
-        </a>
-    )
-}
+  };
+  return (
+    <a href={handleRedirectLink(social)} target="_blank" rel="noreferrer">
+      <div
+        className={`bg-[#1F1F1F] rounded-full text-white flex items-center justify-center cursor-pointer relative h-[35px] gap-3 p-2 ${responsiveClasses}`}
+      >
+        {handleSocialIcon(social)}
+        <span className="hidden md:flex">{social}</span>
+        <span className="hidden lg:flex">
+          <BsArrowUpRight className="top-2 right-4" size={15} />
+        </span>
+      </div>
+    </a>
+  );
+};
 
-export default SocialBadge
+export default SocialBadge;
---
+++ src/pages/about-me/AboutMe.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import "./styles.css";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-2">
+        <div className="h-[100px] border-r border-solid-2 border-gray-900"></div>
+        <div className="h-[100px]"></div>
+      </div>
+      <div className="flex items-center justify-center w-full">
+        <span className="text-center w-[750px] mt-4 text-sm">
+          Highly skilled and adaptable Software Engineer with{" "}
+          <b>more than 1 year </b>
+          of experience in both front-end and back-end development. Proficient
+          in <b>JavaScript, ReactJS, Go, PostgreSQL, and DynamoDB.</b>
+        </span>
+      </div>
+      <div className="grid grid-cols-2 mt-4">
+        <div className="h-[200px] border-r border-solid-2 border-gray-900"></div>
+        <div className="h-[200px]"></div>
+      </div>
+      <div className="flex items-center justify-center relative">
+        <div className="absolute -top-3">
+          <BsChevronDown size={"20px"} />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center mt-8 relative">
+        <span className="text-lg">Hello I am</span>
+        <div className="flex items-center justify-center">
+          <span className="firstname">JOHN PAUL</span>
+        </div>
+        <div className="flex items-center justify-center absolute top-40">
+          <span className="lastname">CERVANTES</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
