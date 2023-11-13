fe8289dc268309ffc79dfd8802fe16e814a08da7
John Paul N. Cervantes <johnpaulncervantes@gmail.com>
2023-11-13 21:59:16 +0800

    added responsiveness to the info page

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -12,13 +12,13 @@ const Info = () => {
       <div className="flex justify-between flex-col pt-20 pb-20">
         <div>
           <span className="title">About Me</span>
-          <div className="w-[450px] bio">
+          <div className="xl:w-[450px] lg:w-[400px] bio lg:text-xs xl:text-sm">
             I have a burning interest for film. My second option for a course to
             take in college was BA Film. My love for tech influenced my love for
             scifi movies.
           </div>
         </div>
-        <div className="flex space-x-6">
+        <div className="flex flex-col xl:space-x-6 xl:flex-row">
           <span className="flex items-center">
             <BsFillTelephoneFill />{" "}
             <span className="ml-2 text-sm"> 09183252038</span>
@@ -33,18 +33,22 @@ const Info = () => {
           </span>
         </div>
       </div>
-      <div className="pr-12 pl-32">
+      <div className="pr-12 pl-32 lg:pr-0">
         <div className="border-b border-t border-black flex flex-col pt-12 pb-12">
           <span className="heading">Education</span>
           <div className="flex justify-between text-lg mt-4">
-            <span>Bulacan State University</span>
-            <span>2018-2022</span>
+            <span className="text-sm lg:text-sm xl:text-lg lg:font-bold">
+              Bulacan State University
+            </span>
+            <span className="text-sm lg:text-sm xl:text-lg">2018-2022</span>
           </div>
           <span className="text-sm">BS Computer Engineering</span>
           <span className="text-sm font-light">President's Lister</span>
           <div className="flex justify-between text-lg mt-5">
-            <span>Bulacan State University</span>
-            <span>2018-2022</span>
+            <span className="text-sm lg:text-sm xl:text-lg lg:font-bold">
+              Bulacan State University
+            </span>
+            <span className="text-sm lg:text-sm xl:text-lg">2018-2022</span>
           </div>
           <span className="text-sm">BS Computer Engineering</span>
           <span className="text-sm font-light">President's Lister</span>
@@ -54,7 +58,9 @@ const Info = () => {
           <span className="heading">Hagonoy, Bulacan</span>
         </div>
         <div className=" border-black flex pt-12 pb-12">
-          <span>an only child</span>
+          <span className="w-fit xl:w-fit lg:w-fit md:w-fit sm:w-fit xs:w-fit">
+            an only child
+          </span>
           <span className="ml-16">
             born on April 7, 2000. I'm an <b>Aries</b>{" "}
           </span>
